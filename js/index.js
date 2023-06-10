49e2bcfe43fe2367bfc8ce9855caa740ade2b266
ashishgur <ashishgurav@mitaoe.ac.in>
2023-06-10 23:27:45 +0530

    new_portfolio

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+
+// window.addEventListener("scroll",function (){
+//     var header=document.querySelector("header");
+//     header.classList.toggle("sticky",window.scrollY >0)
+// })
+
+
+// // for sending mails 
+// function sendmails(){
+//     Email.send({
+//         Host : "smtp.gmail.com.com",
+//         Username : "ashishnew2112@gmail.com",
+//         Password : "password",
+//         To : 'them@website.com',
+//         From : "you@isp.com",
+//         Subject : "This is the subject",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
