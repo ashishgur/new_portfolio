a5aba0603a91e895b97fe975e23d0645c85c9ba7
ashishgur <ashishgurav@mitaoe.ac.in>
2023-07-24 12:48:14 +0530

    update

--- js/index.js
+++ js/index.js
@@ -6,17 +6,3 @@ window.addEventListener("scroll",function (){
 })
 
 
-// // for sending mails 
-// function sendmails(){
-//     Email.send({
-//         Host : "smtp.gmail.com.com",
-//         Username : "ashishnew2112@gmail.com",
-//         Password : "password",
-//         To : 'them@website.com',
-//         From : "you@isp.com",
-//         Subject : "This is the subject",
-//         Body : "And this is the body"
-//     }).then(
-//       message => alert(message)
-//     );
-// }
